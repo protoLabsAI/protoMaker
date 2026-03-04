59413932d965e459051ac646d2cd5cddb2b826db
Josh Mabry <mabry1985@gmail.com>
2026-03-04 11:46:09 -0800

    refactor: Provider types and ClaudeProvider wiring

--- libs/types/src/index.ts
+++ libs/types/src/index.ts
@@ -30,6 +30,9 @@ export type {
   McpHttpServerConfig,
   AgentDefinition,
   ReasoningEffort,
+  HookCallback,
+  HookCallbackMatcher,
+  CanUseTool,
 } from './provider.js';
 
 // Provider constants and utilities
